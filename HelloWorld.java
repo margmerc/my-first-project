ed44cb28f2a31d3e01aea25a9ea07ba5b6cdbe45
margmerc <margaritamercado3@gmail.com>
2026-09-01 20:47:55 -0400

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+package net.javaguides;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+		String name = "Margam";
+		System.out.println(name.toUpperCase());
+		
+	}
+
+	public static void addExclamationPoint(String s) {
+		
+	}
+	
+}
