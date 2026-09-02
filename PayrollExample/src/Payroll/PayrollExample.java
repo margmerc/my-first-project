6708308e29ac6feba2a84153c9258fc8fc76aead
margmerc <margaritamercado3@gmail.com>
2026-09-01 21:04:05 -0400

    Add files via upload

---
+++ PayrollExample/src/Payroll/PayrollExample.java
@@ -0,0 +1,15 @@
+package Payroll;
+
+public class PayrollExample {
+
+	public static void main(String[] args) {
+		
+		int hours = 40;
+		double grossPay, payRate = 25.0;
+				
+		grossPay = hours * payRate;
+		System.out.println ("Your gross pay is $" + grossPay);
+
+	}
+
+}
